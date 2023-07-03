2d8bb5a6dc33c40555bf1bc8c41b62847082b627
Peter Steele <steele.peter.3@gmail.com>
2023-07-03 13:39:24 -0400

    increase width of tablet styles, refactor breakpoints

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices', 'wp-primitives'), 'version' => '81b1153ea309db3b43d5');
+<?php return array('dependencies' => array('react', 'wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-notices', 'wp-primitives'), 'version' => 'b039574a272669354d3c');
